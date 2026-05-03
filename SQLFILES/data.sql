dda070daddf611d2ddb544ed7999e254eb108192
Solemouse <jcdreier2009@gmail.com>
2026-05-03 17:21:46 -0600

    update data.sql

---
+++ SQLFILES/data.sql
@@ -0,0 +1,26 @@
+INSERT INTO Locations (name, address) VALUES
+	('Belmar', '555 S Allison Pkwy, Lakewood, CO 80226'),
+	('Golden', '1019 10th St, Golden, CO 80401'),
+	('Denver', '10 W 14th Ave, Denver, CO 80204'),
+    ('Evergreen', '5000 County Hwy 73, Evergreen, CO 80439'),
+	('Columbine', '7706 W Bowles Ave, Littleton, CO 80123'),
+	('Englewood', '1000 Englewood Pkwy, Englewood, CO 80110'),
+    ('Pueblo', '1315 E 7th St, Pueblo, CO 81001'),
+	('Durango', '1900 E 3rd Ave, Durango, CO 81301'),
+	('Cortez', '202 N Park St, Cortez, CO 81321'),
+    ('Arvada', '7525 W 57th Ave, Arvada, CO 80002');
+
+INSERT INTO Renters (name, num_loans) VALUES
+    ('Jacob Dreier', '5'),
+    ('Owen Milota', '3'),
+    ('Timoty Siebrecht', '8'),
+    ('Aiden Sundermann', '2'),
+    ('Pascal Davis', '1'),
+    ('Grace Person', '6'),
+    ('Evan Santos', '13'),
+    ('Carson Rickard', '4'),
+    ('Ronan Hubbard', '6'),
+    ('Jacob Casey', '1');
+
+
+   
