22e70fbfdbaaf8b26a9a1218a59e932bfc0b8ba3
Solemouse <jcdreier2009@gmail.com>
2026-05-03 16:53:03 -0600

    update schema.sql

--- SQLFILES/schema.sql
+++ SQLFILES/schema.sql
@@ -5,19 +5,22 @@ USE PhysicalMedia;
 CREATE TABLE Locations (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(75) NOT NULL,
-    address VARCHAR(75) NOT NULL
+    address VARCHAR(75) NOT NULL,
+    UNIQUE (name, address)
 );
 
 CREATE TABLE Media_Types (
     id INT AUTO_INCREMENT PRIMARY KEY,
     type VARCHAR(50) NOT NULL,
-    category VARCHAR(50) NOT NULL
+    category VARCHAR(50) NOT NULL,
+    UNIQUE (type)
 );
 
 CREATE TABLE Renters (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    num_loans INT NOT NULL
+    num_loans INT NOT NULL,
+    CHECK (num_loans >= 0)
 );
 
 CREATE TABLE Media_Names (
@@ -27,6 +30,7 @@ CREATE TABLE Media_Names (
     acquiry_date DATE,
     quantity INT,
     type_id INT,
+    CHECK (quantity >= 0),
     FOREIGN KEY (location_id) REFERENCES Locations(id),
     FOREIGN KEY (type_id) REFERENCES Media_Types(id)
 );
@@ -35,6 +39,7 @@ CREATE TABLE Media (
     id INT AUTO_INCREMENT PRIMARY KEY,
     media_type_id INT,
     media_name_id INT,
+    UNIQUE (media_type_id, media_name_id),
     FOREIGN KEY (media_type_id) REFERENCES Media_Types(id),
     FOREIGN KEY (media_name_id) REFERENCES Media_Names(id)
 );
@@ -44,8 +49,9 @@ CREATE TABLE Active_Loans (
     renter_id INT,
     media_id INT,
     home_id INT,
-    check_out_date DATE,
-    loan_expiration DATE,
+    check_out_date DATE DEFAULT (CURRENT_DATE),
+    loan_expiration DATE NOT NULL,
+    CHECK (loan_expiration > check_out_date),
     FOREIGN KEY (renter_id) REFERENCES Renters(id),
     FOREIGN KEY (media_id) REFERENCES Media_Names(id),
     FOREIGN KEY (home_id) REFERENCES Locations(id)
