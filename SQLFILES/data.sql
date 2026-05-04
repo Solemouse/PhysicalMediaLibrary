e14d409eabf0d9725933fc1993e32759c8dafca4
Solemouse <jcdreier2009@gmail.com>
2026-05-03 18:08:45 -0600

    update data.sql

--- SQLFILES/data.sql
+++ SQLFILES/data.sql
@@ -22,5 +22,50 @@ INSERT INTO Renters (name, num_loans) VALUES
     ('Ronan Hubbard', '6'),
     ('Jacob Casey', '1');
 
-
+INSERT INTO Media_Types (type, category) VALUES
+    ('DVD', 'Movie'),
+    ('Blu-Ray', 'Movie'),
+    ('VHS', 'Movie'),
+    ('CD', 'Music'),
+    ('Vinyl', 'Music'),
+    ('Cassette', 'Music'),
+    ('VCD', 'Movie'),
+    ('LaserDisk', 'Movie'),
+    ('8-TrackTape', 'Music'),
+    ('BetaMax', 'Movie');
    
+INSERT INTO Media_Names (location_id, name, acquiry_date, quantity, type_id) VALUES
+    (3, 'Tomorrowland', '2020-03-15', 3, 1),
+    (4, 'Startrek into darkness', '2023-07-20', 4, 2),
+    (7, 'Wallace & Grommit: A Grand Day Out', '1995-04-20', 1, 3),
+    (5, '90125', '2022-04-08', 5, 4),
+    (6, 'Interstellar Album', '2026-01-29', 1, 5),
+    (2, 'Top Gun Soundtrack', '2021-02-15', 2, 6),
+    (8, 'The Simpsons Greatest Hits', '2019-05-18', 3, 7),
+    (9, 'Back to the Future', '1990-07-23', 3, 8),
+    (10, 'Elvis Burning Love', '2001-04-07', 3, 9),
+    (1, 'A Christmas Carol', '1999-02-27', 3, 10);
+
+INSERT INTO Media (media_type_id, media_name_id) VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5),
+    (6, 6),
+    (7, 7),
+    (8, 8),
+    (9, 9),
+    (10, 10);
+
+INSERT INTO Active_Loans (renter_id, media_id, home_id, check_out_date, loan_expiration) VALUES
+    (1, 1, 3, '2024-01-10', '2024-02-10'),
+    (2, 2, 4, '2024-02-15', '2024-03-15'),
+    (3, 3, 7, '2024-03-01', '2024-04-01'),
+    (4, 4, 5, '2024-03-10', '2024-04-10'),
+    (5, 5, 6, '2024-04-01', '2024-05-01'),
+    (6, 6, 2, '2024-04-10', '2024-05-10'),
+    (7, 7, 8, '2024-05-01', '2024-06-01'),
+    (8, 8, 9, '2024-05-15', '2024-06-15'),
+    (9, 9, 10, '2024-06-01', '2024-07-01'),
+    (10, 10, 1, '2024-06-10', '2024-07-10');
